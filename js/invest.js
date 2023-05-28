086abf378151ad52275bda12c8ea598f77b32e9b
Randy Brown <randybrown9812@gmail.com>
2023-05-28 18:00:14 -0500

    Added Form to start implementing Javascript.

--- js/invest.js
+++ js/invest.js
@@ -1,5 +1,9 @@
 let computeButton = document.getElementById("compute");
-
+let resetButton = document.getElementById("reset");
 function test() {
-    window.alert("This button works!");
+    window.alert("This 1st button works!");
+}
+
+function testTwo() {
+    window.alert("This 2nd button works!");
 }
