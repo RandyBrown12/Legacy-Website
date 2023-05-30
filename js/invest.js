aedb384ea143f241f8725284532f71579e9c35b7
Randy Brown <randybrown9812@gmail.com>
2023-05-29 19:25:10 -0500

    Added functionality to buttons using JS.

--- js/invest.js
+++ js/invest.js
@@ -1,9 +1,35 @@
-let computeButton = document.getElementById("compute");
-let resetButton = document.getElementById("reset");
-function test() {
-    window.alert("This 1st button works!");
+const computeButton = document.getElementById("compute");
+const resetButton = document.getElementById("reset");
+const hoursInput = document.getElementById("hours");
+const salaryInput = document.getElementById("salary");
+
+// Events
+computeButton.addEventListener("click", takeHomePay);
+resetButton.addEventListener("click", testTwo);
+
+function takeHomePay() {
+    let hours = parseInt(hoursInput.value);
+    let salary = parseInt(salaryInput.value);
+    try
+    {
+        if(isNaN(hours))
+        {
+            throw "Please enter a number in the hours textbox!";
+        }
+
+        if(isNaN(salary))
+        {
+            throw "Please enter a number in the salary textbox!";
+        }
+        window.alert(hours * salary);
+        hoursInput.value = "";
+        salaryInput.value = "";
+    } catch(e) {
+        window.alert(e);
+    }
 }
 
 function testTwo() {
-    window.alert("This 2nd button works!");
+    hoursInput.value = "";
+    salaryInput.value = "";
 }
