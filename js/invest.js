ac63e60c9a66702e73c11882513665dc0fb8e717
Randy Brown <randybrown9812@gmail.com>
2023-05-30 20:56:43 -0500

    Added another form to be responsive in JS.

--- js/invest.js
+++ js/invest.js
@@ -1,35 +1,60 @@
-const computeButton = document.getElementById("compute");
-const resetButton = document.getElementById("reset");
 const hoursInput = document.getElementById("hours");
 const salaryInput = document.getElementById("salary");
-
+const computeButton = document.getElementById("compute");
+const resetButton = document.getElementById("reset");
+const taxRateOutput = document.getElementById("taxRate");
+const takeHomePayOutput = document.getElementById("takeHomePay");
+const conversionRatio = 12;
+var taxRate = 0;
+var grossIncome = 0;
 // Events
 computeButton.addEventListener("click", takeHomePay);
 resetButton.addEventListener("click", testTwo);
 
 function takeHomePay() {
-    let hours = parseInt(hoursInput.value);
+    grossIncome = 0;
+    taxRate = 0;
+    taxRateOutput.textContent = taxRateOutput.textContent.substring(0, 9);
+    takeHomePayOutput.textContent = takeHomePayOutput.textContent.substring(0, 24);
+/*     let years = parseInt(hoursInput.value) * 8760; */
+    console.log(taxRateOutput.textContent);
     let salary = parseInt(salaryInput.value);
     try
     {
-        if(isNaN(hours))
+/*         if(isNaN(years))
         {
             throw "Please enter a number in the hours textbox!";
-        }
-
+        } */
         if(isNaN(salary))
         {
             throw "Please enter a number in the salary textbox!";
         }
-        window.alert(hours * salary);
-        hoursInput.value = "";
         salaryInput.value = "";
     } catch(e) {
         window.alert(e);
     }
+    grossIncome = salary;
+    taxRate = getTaxRate(grossIncome);
+    taxRateOutput.textContent += " " + parseFloat(taxRate).toFixed(2)+"%";
+    takeHomePayOutput.textContent += " " + Math.round(grossIncome / conversionRatio) + "/month";
 }
 
 function testTwo() {
-    hoursInput.value = "";
     salaryInput.value = "";
+}
+
+function getTaxRate(useGrossIncome) {
+    if(useGrossIncome > 215950) {
+        return 0.35;
+    } else if(useGrossIncome > 170050) {
+        return 0.32;
+    } else if(useGrossIncome > 89075) {
+        return 0.24;
+    } else if(useGrossIncome > 41775) {
+        return 0.22;
+    } else if(useGrossIncome > 10275) {
+        return 0.12;
+    } else {
+        return 0;
+    }
 }
