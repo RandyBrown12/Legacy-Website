c030c7cfcb37ad706d92ee1b8ed4db05d25d2eea
Randy Brown <randybrown9812@gmail.com>
2023-06-03 17:35:54 -0500

    Converted numbers in inputs to values outputted.

--- js/invest.js
+++ js/invest.js
@@ -4,42 +4,53 @@ const computeButton = document.getElementById("compute");
 const resetButton = document.getElementById("reset");
 const taxRateOutput = document.getElementById("taxRate");
 const takeHomePayOutput = document.getElementById("takeHomePay");
-const conversionRatios = [["hour", 1], ["week", 168], ["month", 1], ["year", 1]];
+const optionBox = document.getElementById("timeConverter");
+//converted to years.
+const conversionRatios = new Map([["Week", 52.1429], ["Month", 4.34524], ["Year", 1]]);
 let taxRate = 0;
 let grossIncome = 0;
+let conversionOption = "";
 
 // Events
 computeButton.addEventListener("click", takeHomePay);
 resetButton.addEventListener("click", testTwo);
 
 //Note: First input is 40 hours per week.
-
+//Note 2: If in hours, convert hours to weeks otherwise convert [weeks,months,years] to years.
+//Test: $7.25 hr with 40 hrs/week should be 290 per week.
+//Test2: $290 week with 40 hrs/week should be 1256 per month.
 function takeHomePay() {
-    grossIncome = 0;
     taxRate = 0;
+    grossIncome = 0;
     taxRateOutput.textContent = taxRateOutput.textContent.substring(0, 9);
     takeHomePayOutput.textContent = takeHomePayOutput.textContent.substring(0, 24);
-/*     let years = parseInt(hoursInput.value) * 8760; */
-    console.log(taxRateOutput.textContent);
-    let salary = parseInt(salaryInput.value);
+    let workHours = parseInt(hoursInput.value);
+    let salary = parseFloat(salaryInput.value);
+    let conversionOption = timeConverter.options[timeConverter.selectedIndex].text;
     try
     {
-/*         if(isNaN(years))
+        if(isNaN(workHours))
         {
             throw "Please enter a number in the hours textbox!";
-        } */
+        }
         if(isNaN(salary))
         {
             throw "Please enter a number in the salary textbox!";
         }
         salaryInput.value = "";
+        hoursInput.value = "";
     } catch(e) {
         window.alert(e);
     }
-    grossIncome = salary;
+
+    if(conversionOption === "Hour") {
+        salary *= workHours;
+        conversionOption = "Week";
+    }
+    grossIncome = salary * conversionRatios.get(conversionOption);
     taxRate = getTaxRate(grossIncome);
     taxRateOutput.textContent += " " + (taxRate * 100) + "%";
-    takeHomePayOutput.textContent += " " + Math.round(grossIncome / 180) + "/month";
+    takeHomePayOutput.textContent += " " + Math.round(grossIncome / 12) + "/month";
 }
 
 function testTwo() {
