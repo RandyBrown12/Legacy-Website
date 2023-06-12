40655deb016badab51d4a4b51603564fc57c9f2c
Randy Brown <randybrown9812@gmail.com>
2023-06-12 17:34:58 -0500

    Updated State Tax function to be more reflective.

--- js/invest.js
+++ js/invest.js
@@ -2,20 +2,19 @@ const hoursInput = document.getElementById("hours");
 const salaryInput = document.getElementById("salary");
 const computeButton = document.getElementById("compute");
 const resetButton = document.getElementById("reset");
-const taxRateOutput = document.getElementById("taxRate");
+const federalTaxRateOutput = document.getElementById("federalTaxRate");
+const stateTaxRateOutput = document.getElementById("stateTaxRate");
 const takeHomePayOutput = document.getElementById("takeHomePay");
 const optionBox = document.getElementById("timeConverter");
 //converted to years.
 const conversionRatios = new Map([["Week", 52.1429], ["Month", 4.34524], ["Year", 1]]);
 //Margin Tax Rate: $10,275 goes to 10%, $31500 goes to 12%, until it reaches a bracket.
-const federalYearlyRates = [[0.10, 10275], [0.12, 41775], [0.22, 89075], [0.24, 170050], [0.32, 215950], [0.35, 539900], [0.37, 999999999]];
-const stateYearlyRates = [[0, 0, 0], [6350, 0, 0.25], [7350, 2.50, 0.75], [8,850, 13.75, 1.75], [10100, 35.63, 2.75], [11250, 67.25, 3.75], [13550, 153.50, 4.75]];
+//1st additional money, 2nd number rate, 3rd number maxAllotedIncome
+const federalYearlyRates = [[0.10, 10275], [0.12, 41775], [0.22, 89075], [0.24, 170050], [0.32, 215950], [0.35, 539900], [0.37, Infinity]];
+const stateYearlyRates = [[0, 0, 6350], [0, 0.0025, 7350], [2.50, 0.0075, 8850], [13.75, 0.0175, 10100], [35.63, 0.0275, 11250], [67.25, 0.0375, 13550], [153.50, 0.0475, Infinity]];
 const fica = [[0.062, 147000],[0.0145, 0]];
 //1825 Semi-Monthly. Two Withholding
 //41.67 * 2
-let taxRate = 0;
-let grossIncome = 0;
-let conversionOption = "";
 
 // Events
 computeButton.addEventListener("click", takeHomePay);
@@ -26,22 +25,19 @@ optionBox.addEventListener("change", selectedOption);
 //Test: $7.25 hr with 40 hrs/week should be 290 per week.
 //Test2: $290 week with 40 hrs/week should be 1256 per month.
 function takeHomePay() {
-    taxRate = 0;
-    grossIncome = 0;
-    taxRateOutput.textContent = taxRateOutput.textContent.substring(0, 9);
+    let taxRate = 0, grossIncome = 0, federalTaxedIncome = 0, stateTaxedIncome = 0;
+    let conversionOption = timeConverter.options[timeConverter.selectedIndex].text;
+    federalTaxRateOutput.textContent = federalTaxRateOutput.textContent.substring(0, 19);
+    stateTaxRateOutput.textContent = stateTaxRateOutput.textContent.substring(0, 17);
     takeHomePayOutput.textContent = takeHomePayOutput.textContent.substring(0, 24);
     let workHours = parseInt(hoursInput.value);
     let salary = parseFloat(salaryInput.value);
-    let conversionOption = timeConverter.options[timeConverter.selectedIndex].text;
+
     try
     {
-        if(isNaN(workHours) && timeConverter.selectedIndex === 0)
+        if((isNaN(workHours) && conversionOption === "Hour")|| isNaN(salary))
         {
-            throw "Please enter a number in the hours textbox!";
-        }
-        if(isNaN(salary))
-        {
-            throw "Please enter a number in the salary textbox!";
+            throw "Please enter numbers in the textboxes!";
         }
         resetTextboxes();
     } catch(e) {
@@ -53,9 +49,11 @@ function takeHomePay() {
         conversionOption = "Week";
     }
     salary *= conversionRatios.get(conversionOption);
+    federalYearlyRates[6][1] = stateYearlyRates[6][2] = salary;
     federalTaxedIncome = getFederalTaxRate(salary);
     stateTaxedIncome = getStateTaxRate(salary);
-    taxRateOutput.textContent += (federalTaxedIncome / salary).toFixed(2) + "    " + (stateTaxedIncome / salary).toFixed(2);
+    federalTaxRateOutput.textContent += " " + federalTaxedIncome;
+    stateTaxRateOutput.textContent += " " + stateTaxedIncome;
     takeHomePayOutput.textContent += " " + Math.round(salary / 12) + "/month";
 }
 
@@ -75,34 +73,32 @@ function selectedOption() {
 
 function getFederalTaxRate(useGrossIncome) {
     let prevMaxIncome = 0;
-    let federalTaxes = 0;
-    for([marginalTaxRate, maxIncome] of federalYearlyRates)
+    let taxes = 0;
+    for([marginalTaxRate, maxAllotedIncome] of federalYearlyRates)
     {
-        if(useGrossIncome > maxIncome)
+        if(useGrossIncome > maxAllotedIncome)
         {
-            federalTaxes += marginalTaxRate * (maxIncome - prevMaxIncome);
-            prevMaxIncome = maxIncome;
+            taxes += marginalTaxRate * (maxAllotedIncome - prevMaxIncome);
+            prevMaxIncome = maxAllotedIncome;
         } else {
-            federalTaxes += marginalTaxRate * (useGrossIncome - prevMaxIncome);
+            taxes += marginalTaxRate * (useGrossIncome - prevMaxIncome);
             break;
         }
     }
-    return federalTaxes.toFixed(2);
+    return taxes.toFixed(2);
 }
 
 function getStateTaxRate(useGrossIncome) {
     let prevMaxIncome = 0;
-    let federalTaxes = 0;
-    for([marginalTaxRate, maxIncome] of stateYearlyRates)
+    let taxes = 0;
+    for([additionalIncome, marginalTaxRate, maxAllotedIncome] of stateYearlyRates)
     {
-        if(useGrossIncome > maxIncome)
-        {
-            federalTaxes += marginalTaxRate * (maxIncome - prevMaxIncome);
-            prevMaxIncome = maxIncome;
-        } else {
-            federalTaxes += marginalTaxRate * (useGrossIncome - prevMaxIncome);
-            break;
+        if(useGrossIncome > maxAllotedIncome) 
+        { 
+            prevMaxIncome = maxAllotedIncome
+            continue;
         }
+        taxes = additionalIncome + (marginalTaxRate * (useGrossIncome - prevMaxIncome));
     }
-    return stateTaxes.toFixed(2);
+    return taxes.toFixed(2);
 }
